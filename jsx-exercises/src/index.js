101cd33032002e582125c1664df083462a19cf1a
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-10 13:25:43 +0200

    es fixato

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const name = 'Andrea Sabato'
-const sayHello = <h1>Hello, {name}</h1>
-
-ReactDOM.render(sayHello,document.getElementById('root'));
+const greetings = (name) => {
+    return <h1>Hello, {name}</h1>
+} 
