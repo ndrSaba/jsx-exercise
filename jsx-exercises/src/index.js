757cdf9ab366303d7c971c4e53f137f619b3798a
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-10 13:28:02 +0200

    es fixato

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const sum = (a, b) => a + b
-const result = <h2>{sum(10, 10)}</h2>
-
-ReactDOM.render(result,document.getElementById('root'));
+const sum = (a, b) => {
+    return <h2> {a + b} </h2>
+}
